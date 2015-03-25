a96d576a2a282d89078c62bd9159cf445fa5615c
Lars Thorup <lars@staance.com>
2015-03-25 09:49:16 -0600

    including weather in demo page

--- src/js/main.js
+++ src/js/main.js
@@ -8,8 +8,10 @@ require(['require.conf'], function () {
     require(['css!style/menu'], function () {
 
         // Note: finally load markup into the DOM
-        require(['jquery', 'text!template/menu.html'], function ($, menu) {
-            $('body').append(menu);
+        require(['jquery', 'text!template/menu.html', 'text!template/weather.html', 'js/page/weather'], function ($, menuHtml, weatherHtml, weather) {
+            $('body').append(menuHtml);
+            $('body').append(weatherHtml);
+            weather.listen($('#weather'));
         });
 
     });
